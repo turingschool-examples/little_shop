8a93e7523cee900a89cbe6ce322b81ae322c95c6
username <wnt925@gmail.com>
2019-07-02 14:27:11 -0600

    Add within to the index test

--- spec/features/reviews/delete_spec.rb
+++ spec/features/reviews/delete_spec.rb
@@ -1,7 +1,8 @@
-  require 'rails_helper'
+require 'rails_helper'
 
-  RSpec.describe 'Review Index Page' do
-    describe 'As a visitor' do
+RSpec.describe 'Delete Item' do
+  describe 'As a Visitor' do
+    describe 'When I visit an items show page' do
       before :each do
         @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
         @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
@@ -9,15 +10,18 @@
         @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
         @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
         @review_1 = @hippo.reviews.create!(title: 'Good Hippo', content: "The Hippo was good", rating: 4 )
+        @review_2 = @hippo.reviews.create!(title: 'bad Hippo', content: "The Hippo was bad", rating: 1 )
       end
 
-        it 'I can see a list of all items' do
+
+      xit 'I can click a link to delete that item' do
         visit "/items/#{@hippo.id}"
-        within "#review-#{@review_1.id}" do
-          expect(page).to have_content(@review_1.title)
-          expect(page).to have_content(@review_1.content)
-          expect(page).to have_content(@review_1.rating)
-        end
+
+
+        expect(current_path).to eq("/items/#{@hippo.id}")
+        click_link 'Delete'
+
       end
-    end 
+    end
   end
+end
