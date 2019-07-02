4c33c7f09bad0e34964c7549db2cd7e087088ec0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-02 14:50:23 -0600

    Add cart index route

--- spec/features/cart/index_spec.rb
+++ spec/features/cart/index_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe "When a user adds an item to their cart" do
     megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
     item_1 = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
     item_2 = megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-    binding.pry
 
     visit "/items"
 
