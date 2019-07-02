8f63fcd978419f9d07d41cce7fde22b139f15aa1
Jake Miller <jake0miller@gmail.com>
2019-07-02 14:40:07 -0600

    Update cart to add items from show page and redirect to items index

--- spec/features/items/merchant_index_spec.rb
+++ spec/features/items/merchant_index_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe 'Merchant Item Index Page' do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-    end
-    it 'I can see a list of that merchants items' do
       visit "/merchants/#{@megan.id}/items"
+    end
 
+    it 'I can see a list of that merchants items' do
       within "#item-#{@ogre.id}" do
         expect(page).to have_link(@ogre.name)
         expect(page).to have_content(@ogre.description)
@@ -34,6 +34,16 @@ RSpec.describe 'Merchant Item Index Page' do
       end
 
       expect(page).to_not have_css("#item-#{@hippo.id}")
+    end
+
+    it 'can see a link to the merchant' do
+      within 'h1' do
+        expect(page).to have_link("Megan")
+
+        click_link "Megan"
+
+        expect(current_path).to eq("/merchants/#{@megan.id}")
+      end
     end
   end
 end
