13bfc40a65fde3ec1d0ecc9b06257a893caa7e35
Jake Miller <jake0miller@gmail.com>
2019-07-03 14:26:57 -0600

    Write restrictions on valid zip codes and refactor path helpers

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,38 +1,52 @@
 class MerchantsController < ApplicationController
+  before_action :set_merchant, only: [:show, :edit, :update, :destroy]
+
   def index
     @merchants = Merchant.all
   end
 
   def show
-    @merchant = Merchant.find(params[:id])
   end
 
   def new
+    @merchant = Merchant.new
   end
 
   def create
-    Merchant.create(merchant_params)
-    redirect_to '/merchants'
+    @merchant = Merchant.new(merchant_params)
+    if @merchant.zip.to_s.length != 5 || @merchant.zip.to_s != @merchant.zip.to_i.to_s
+      flash[:notice] = "Merchant not created! Bad zip code."
+      render :new
+    else
+      if @merchant.save
+        redirect_to merchants_path
+      else
+        flash[:notice] = "Merchant not created! Missing information."
+        render :new
+      end
+    end
   end
 
   def edit
-    @merchant = Merchant.find(params[:id])
   end
 
   def update
-    @merchant = Merchant.find(params[:id])
     @merchant.update(merchant_params)
-    redirect_to "/merchants/#{@merchant.id}"
+    redirect_to merchant_path(@merchant)
   end
 
   def destroy
-    Merchant.destroy(params[:id])
-    redirect_to '/merchants'
+    @merchant.destroy
+    redirect_to merchants_path
   end
 
   private
 
   def merchant_params
     params.permit(:name, :address, :city, :state, :zip)
+  end
+
+  def set_merchant
+    @merchant ||= Merchant.find(params[:id])
   end
 end
