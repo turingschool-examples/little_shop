e8ae8950eced8aae985114b73e493f67a7e48bf2
Jake Miller <jake0miller@gmail.com>
2019-07-03 12:33:36 -0600

    Refactor model tests to include validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Item do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :price}
+    it {should validate_presence_of :image}
+    it {should validate_presence_of :inventory}
+  end
 end
