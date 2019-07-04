6c0fd20ad218bb65be7af3901d50cc284b26bb44
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-03 20:29:40 -0600

    Add index method in CartController
    Co-Authored-By: Tay James <tayjames@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,11 +2,15 @@ class CartController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def add_item
-    item = Item.find(params[:item_id]) #ask instructors about this!!!
-    cart = Cart.new(session[:cart])
+    item = Item.find(params[:item_id])
+    # cart = Cart.new(session[:cart])
     cart.add_item(item.id)
     session[:cart] = cart.contents
     flash[:success] = "#{item.name} has been added to your cart."
     redirect_to items_path
+  end
+
+  def index
+    @items = Item.find(cart.item_ids)
   end
 end
