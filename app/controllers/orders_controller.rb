e81aeb23166eaffc930501ac4bf727f4ace58728
Jori Peterson <joripeterson@gmail.com>
2019-07-06 13:45:08 -0600

    Refactor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,17 +14,14 @@ class OrdersController < ApplicationController
     end
   end
 
-
   def create
     order = Order.create(order_params)
 
     redirect_to "/orders/#{order.id}"
   end
 
-
   private
   def order_params
     params.permit(:name, :address, :city, :state, :zipcode)
   end
-
 end
--- spec/features/orders/new_spec.rb
+++ spec/features/orders/new_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe 'New Order' do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-
       @cart = Cart.new(nil)
-
     end
+
     describe 'When I checkout from my cart' do
       it 'I see the details the detail of my cart' do
 
@@ -37,6 +36,7 @@ RSpec.describe 'New Order' do
         expect(page).to have_button 'Create Order'
       end
     end
+    
     describe 'When I click on "Create Order" an order is created' do
       it "I am redirected to that order's show page" do
 
