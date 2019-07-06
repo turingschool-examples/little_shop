0f660365033227dd5ba95e17fcd9bd34fc452847
Jake Miller <jake0miller@gmail.com>
2019-07-06 17:30:13 -0600

    Change ruby methods to AR methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def self.top_three_items
+    Merchant.items.order
+  end
 end
