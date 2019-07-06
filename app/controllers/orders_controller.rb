c84055c3ff1ccbe97b34c72f6a9b8e87b16f3950
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-06 09:50:48 -0600

    Add methods to OrdersController: new, create, show, order_params
    Co-Authored-By: Tay James <tayjames@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,28 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @items = Item.find(cart.item_ids)
+  end
 
+  def create
+    order = Order.new(order_params)
+    if order.save
+      flash[:success] = "Your order has been created."
+      redirect_to "/orders/#{order.id}"
+    else
+      flash[:error] = "Order not created. You are missing required field(s)."
+      render :new
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @items = Item.find(cart.item_ids)
+  end
+
+  private
+
+  def order_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 end
