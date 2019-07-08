ae764efca9f3e076f5a78f1a7fa3152a164bd4bc
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-08 10:50:56 -0600

    Add method to create orders with flash messages for required information.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,4 +10,34 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def create
+    @order = Order.new(order_params)
+    # session[:cart].each do |key, qty|
+    cart.contents.each do |key, qty|
+      item = Item.find(key.to_i)
+      OrderItem.create(order_id: @order, item_id: item, count: qty, amount: item.price)
+      # OrderItem.create!(order_id: @order.id, item_id: item, amount: item.price, count: qty) Do i want the bang?
+    end
+    if @order.name == nil
+      flash[:error] = "Shipping Information is required in all fields. Please input a name."
+      render :new
+    elsif @order.address == nil
+      flash[:notice] = "Shipping Information requires an address. Please input an address."
+      render :new
+    elsif @order.city == nil
+      flash[:notice] = "Shipping Information requires a city. Please input a city."
+      render :new
+    elsif @order.state == nil
+      flash[:notice] = "Shipping Information requires a state. Please input a state."
+      render :new
+    elsif @order.zip == nil
+      flash[:notice] = "Shipping Information requires a valid zipcode. Please input a zipcode."
+      render :new
+    else
+      @order.save
+      session.delete(:cart)
+      redirect_to order_path
+    end
+  end
+
 end
