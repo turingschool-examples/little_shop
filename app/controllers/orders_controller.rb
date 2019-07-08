f82053f69854dcf7f849ac5c1c52a27b13d44a3b
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-08 10:46:12 -0600

    create new method for orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,10 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def new
+    @items = cart.items
+    #should order items be created here right when there is a new instance of order?
+    @order = Order.new
+  end
+
 end
