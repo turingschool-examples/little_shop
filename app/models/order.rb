abdbf2d6b36eb39354591fb80c128cabdfac3010
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-08 10:57:30 -0600

    Create class for orders.

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ApplicationRecord
+  has_many :order_items
+  has_many :items, through: :order_items
+  belongs_to :merchant
+  validates_presence_of :name, :address, :city, :state, :zip
+
+end
