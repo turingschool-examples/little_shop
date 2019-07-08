4e94f1bfaba8d7b63e1c26b8cd5efda2ceb2e3e0
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-08 13:43:36 -0600

    Add known needed methods for merchant class statistics

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,15 @@
 class Merchant < ApplicationRecord
   has_many :items
+  validates_presence_of :name, :address, :city, :state, :zip
+
+  # def total_inventory  
+  # end
+  #
+  # def ave_item_price
+  # end
+  #
+  # def cities
+  # end
+
+
 end
