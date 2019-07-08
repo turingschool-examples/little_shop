528b69f1f261a41b7c6745c62410a84d9b29a84a
username <wnt925@gmail.com>
2019-07-08 01:50:42 -0600

    Add update to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,11 +22,11 @@ class ReviewsController < ApplicationController
       @review = Review.find(params[:id])
     end
 
-    # def update
-    #   review = Review.find(params[:id])
-    #   review.update(review_params)
-    #   redirect_to "/items/#{review.item_id}"
-    # end
+    def update
+      review = Review.find(params[:id])
+      review.update(review_params)
+      redirect_to "/items/#{review.item_id}"
+    end
 
     def destroy
       Review.destroy(params[:id])
