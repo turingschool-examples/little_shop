df33a86695bc87a11aaeaa962084110fefd90297
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-07-08 17:21:28 -0600

    Add cart model tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Cart do
   describe 'instance methods' do
+    before(:each) do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+      @cart = Cart.new(nil)
+      @cart.add_item(@ogre.id)
+      @cart.add_item(@giant.id)
+      @cart.add_item(@giant.id)
+      @cart.add_item(@ogre.id)
+      @cart.add_item(@hippo.id)
+    end
+
     describe '#contents' do
       it 'should return an empty hash when the contents are nil' do
         cart = Cart.new(nil)
@@ -12,21 +27,82 @@ RSpec.describe Cart do
         expect(cart.contents).to eq ({"12" => 2, "112" => 1})
       end
       describe '#add_item' do
-        it 'adds items' do
-          cart = Cart.new(nil)
-          cart.add_item(1)
-          cart.add_item(2)
-          cart.add_item(2)
-          cart.add_item(1)
-          cart.add_item(3)
+        it 'adds items to the cart' do
 
           expected = {
-            "1" => 2,
-            "2" => 2,
-            "3" => 1
+            @ogre.id.to_s => 2,
+            @giant.id.to_s => 2,
+            @hippo.id.to_s => 1
           }
 
-          expect(cart.contents).to eq(expected)
+          expect(@cart.contents).to eq(expected)
+          end
+        end
+
+        describe '#item_count' do
+          it 'should display count of items in the cart' do
+
+            expect(@cart.item_count(@ogre.id)).to eq(2)
+            expect(@cart.item_count(@giant.id)).to eq(2)
+            expect(@cart.item_count(@hippo.id)).to eq(1)
+          end
+        end
+
+        describe '#total' do
+          it 'displays the total items in the cart' do
+
+          expect(@cart.total).to eq(5)
+        end
+      end
+
+      describe '#subtotal' do
+        it 'displays the total price for each item in the cart' do
+
+        expect(@cart.subtotal(@ogre.id)).to eq(40)
+        expect(@cart.subtotal(@giant.id)).to eq(100)
+        expect(@cart.subtotal(@hippo.id)).to eq(50)
+        end
+      end
+
+      describe '#grand_total' do
+        it 'displays the total price for all items in the cart combined' do
+
+        expect(@cart.grand_total).to eq(190)
+        end
+      end
+
+      describe '#empty?' do
+        it 'returns true if empty else false' do
+
+          expect(@cart.empty?).to eq(false)
+        end
+      end
+
+      describe '#remove_item' do
+        it 'removes an item from the cart' do
+
+          expected = {
+            @giant.id.to_s => 2,
+            @hippo.id.to_s => 1
+          }
+
+          @cart.remove_item(@ogre.id.to_s)
+          expect(@cart.contents).to eq(expected)
+        end
+      end
+
+      describe '#update_quantity' do
+        it 'updates the quantity of items in the cart' do
+
+          expected = {
+            @ogre.id.to_s => 2,
+            @giant.id.to_s => 2,
+            @hippo.id.to_s => 3
+          }
+
+          @cart.update_quantity(@hippo.id.to_s, 3)
+
+          expect(@cart.contents).to eq(expected)
         end
       end
     end
