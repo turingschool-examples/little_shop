89f0a628d7c4c704a7a932c13630b73062159caa
Sejin Kim <froydroyce@gmail.com>
2019-07-09 17:50:14 -0600

    Add merchant stats functionality

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,11 +8,20 @@ class Merchant < ApplicationRecord
   validates :zip, numericality: { message: "must be valid."}
 
   def merchant_orders
+    #items.joins(:orders).empty?
     orders = Order.all
     orders.flat_map do |order|
       order.items.map do |item|
         item.merchant_id.to_s
       end
     end
+  end
+
+  def average_price
+    items.average(:price)
+  end
+
+  def cities
+    items.joins(:orders).pluck('orders.city')
   end
 end
