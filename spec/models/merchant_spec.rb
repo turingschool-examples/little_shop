ee04ab0b5f1a4f9f39ba39ea2a211fce72e33c9f
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-09 22:31:18 -0600

    Make test body for total inventory and change giant price

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Merchant do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
-      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 40, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
       @order_1 = Order.create!(name: 'Jake', address: '123 froggo street', city: 'Greenbay', state: 'WI', zip: 22908)
         @order_1.add_items({@ogre => 3, @giant => 1})
@@ -21,7 +21,8 @@ RSpec.describe Merchant do
 
   describe "total_inventory" do
     it "Check the total inventory of a merchant in an order" do
-
+      expect(@megan.total_inventory).to eq(4)
+      expect(@brian.total_inventory).to eq(9)
     end
   end
 
