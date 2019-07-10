25e26d3032fe54bb9c533a05681a99ed2775c87e
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-07-09 23:25:37 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,13 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create]
   end
 
-
-
-  # resources :cart, only: [:update, :show, :destroy, : :update]
-
-
+  resources :orders, only: [:new, :create, :show]
 
   post '/cart/:item_id', to: 'cart#add_item', as: 'add_cart'
   get '/cart', to: 'cart#show', as: 'cart'
   delete '/cart', to: 'cart#destroy', as: 'empty_cart'
   put '/cart/:item_id', to: 'cart#remove_item', as: 'remove_item'
   patch '/cart/:item_id', to: 'cart#update', as: 'update_cart'
-
-  resources :orders, only: [:new, :create, :show]
-
 
   get "/items/:item_id/reviews/new", to: 'reviews#new', as: 'reviews_new_path'
   post '/items/:item_id', to: 'reviews#create'
