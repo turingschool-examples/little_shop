2c03905709c11c1743ad7a77edaf77f188673923
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-09 22:21:45 -0600

    Create Orders and add items to orders in merchant method test setup

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe Merchant do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
-
+      @order_1 = Order.create!(name: 'Jake', address: '123 froggo street', city: 'Greenbay', state: 'WI', zip: 22908)
+        @order_1.add_items({@ogre => 3, @giant => 1})
+      @order_2 = Order.create!(name: 'Kelly', address: '123 doggo street', city: 'Naples', state: 'FL', zip: 34109)
+        @order_2.add_items({@hippos => 9})
     end
   end
 
