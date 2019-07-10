e46a8adcb89586b80a6fc848a9f163dc04c12fb1
Kelly Sandeson <kellysandeson@gmail.com>
2019-07-09 21:30:30 -0600

    Create total inventory method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,14 +2,10 @@ class Merchant < ApplicationRecord
   has_many :items
   validates_presence_of :name, :address, :city, :state, :zip
 
-  # def total_inventory  
-  # end
-  #
-  # def ave_item_price
-  # end
-  #
-  # def cities
-  # end
+  def total_inventory
+    self.items.count
+    binding.pry
+  end
 
 
 end
