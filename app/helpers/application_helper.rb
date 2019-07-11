ee7ba82cd31c626a21f8b1de843dec86b89508f0
Jake Miller <jake0miller@gmail.com>
2019-07-11 00:10:52 -0600

    Color flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
-  def flash_class(level)
-    case level
-      when :notice then "alert-info"
-      when :success then "alert-success"
-      when :error then "alert-error"
-    end
+  def flash_class(type)
+    type.to_s
   end
 end
