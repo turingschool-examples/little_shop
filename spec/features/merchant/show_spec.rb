b36dfd76aa9c5966c1d3d6bc47f22fd72273e2e2
Sejin Kim <froydroyce@gmail.com>
2019-07-10 18:45:56 -0600

    Add link to merchants items

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Merchant Show Page' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       visit merchant_path(@megan)
     end
 
@@ -17,7 +18,7 @@ RSpec.describe 'Merchant Show Page' do
     end
 
     it 'I see a link to this merchants items' do
-      expect(page).to have_link('Items')
+      expect(page).to have_link('1')
     end
   end
 end
